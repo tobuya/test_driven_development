0ba2fac9776c68fc66967d58e4f0e52fb754f55b
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-24 18:38:54 +0500

    Add fizzbuzz method tests
    Co-authored-by: tobuya <thomasobuya95@gmail.com>
    Co-authored-by: SabaAhmad404 <sabaahmad895@gmail.com>

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -30,4 +30,19 @@ RSpec.describe Solver do
       expect(subject.reverse('hello world')).to eq('dlrow olleh')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when given 9' do
+        expect(subject.fizzbuzz(9)).to eq("fizz")
+    end
+    it 'returns "buzz" when given 20' do
+        expect(subject.fizzbuzz(20)).to eq("buzz")
+    end
+    it 'returns "fizzbuzz" when given 15' do
+        expect(subject.fizzbuzz(15)).to eq("fizzbuzz")
+    end
+    it 'returns 7 when given 7' do
+        expect(subject.fizzbuzz(7)).to eq(7)
+    end
+end
 end
